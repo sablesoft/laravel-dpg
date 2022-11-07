608a2b3fd384967a2d85c67e98f6ea71b3870ac4
Raman Sobal <v-romansobol@4over.com>
2022-11-07 17:09:20 -0300

    update footer

--- resources/views/vendor/nova/partials/footer.blade.php
+++ resources/views/vendor/nova/partials/footer.blade.php
@@ -1,7 +1,6 @@
 <p class="mt-8 text-center text-xs text-80">
-    <a href="https://nova.laravel.com" class="text-primary dim no-underline">Laravel Nova</a>
+    <a href="http://167.99.39.91/dpg" class="text-primary dim no-underline">DPG</a>
     <span class="px-1">&middot;</span>
-    &copy; {{ date('Y') }} Laravel LLC - By Taylor Otwell, David Hemphill, and Steve Schoger.
+    &copy; {{ date('Y') }} Deck Playing Game
     <span class="px-1">&middot;</span>
-    v{{ \Laravel\Nova\Nova::version() }}
 </p>

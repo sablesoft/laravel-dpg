82cecd4eca54af9cda51e21ad237e504033ec251
Raman Sobal <v-romansobol@4over.com>
2022-11-08 20:58:52 -0300

    create content index query

--- app/Nova/Content.php
+++ app/Nova/Content.php
@@ -3,6 +3,8 @@
 namespace App\Nova;
 
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Builder;
+use Laravel\Nova\Http\Requests\NovaRequest;
 use App\Nova\Filters\TagsFilter;
 use App\Nova\Filters\OwnersFilter;
 use App\Nova\Filters\IsPublicFilter;
@@ -24,6 +26,24 @@ abstract class Content extends Resource
     public static $search = [
         'name'
     ];
+
+
+    /**
+     * Build an "index" query for the given resource.
+     *
+     * @param NovaRequest $request
+     * @param  Builder  $query
+     * @return Builder
+     */
+    public static function indexQuery(NovaRequest $request, $query): Builder
+    {
+        if ($request->user()->hasRole(\App\Models\User::ROLE_ADMIN)) {
+            return $query;
+        }
+
+        return $query->where('is_public', '=', true)
+            ->orWhere('owner_id', '=', $request->user()->getKey());
+    }
 
 
     /**

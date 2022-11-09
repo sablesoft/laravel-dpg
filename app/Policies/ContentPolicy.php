bbb0fe13253057e247921425f286c19b8c45ed23
Raman Sobal <v-romansobol@4over.com>
2022-11-08 21:31:01 -0300

    update content policy

--- app/Policies/ContentPolicy.php
+++ app/Policies/ContentPolicy.php
@@ -84,4 +84,100 @@ abstract class ContentPolicy
     {
         return $user->isAdmin() || $user->isOwner($content);
     }
+
+    /**
+     * Determine whether the user can attach any tags to the content.
+     *
+     * @param User $user
+     * @param Content $content
+     * @return bool
+     */
+    public function attachAnyTag(User $user, Content $content): bool
+    {
+        return $user->isAdmin() || $user->isOwner($content);
+    }
+
+    /**
+     * Determine whether the user can add tag to the content.
+     *
+     * @param User $user
+     * @param Content $content
+     * @return bool
+     */
+    public function addTag(User $user, Content $content): bool
+    {
+        return $user->isAdmin() || $user->isOwner($content);
+    }
+
+    /**
+     * Determine whether the user can attach any card to the content.
+     *
+     * @param User $user
+     * @param Content $content
+     * @return bool
+     */
+    public function attachAnyCard(User $user, Content $content): bool
+    {
+        return $user->isAdmin() || $user->isOwner($content);
+    }
+
+    /**
+     * Determine whether the user can add card to the content.
+     *
+     * @param User $user
+     * @param Content $content
+     * @return bool
+     */
+    public function addCard(User $user, Content $content): bool
+    {
+        return $user->isAdmin() || $user->isOwner($content);
+    }
+
+    /**
+     * Determine whether the user can attach any deck to the content.
+     *
+     * @param User $user
+     * @param Content $content
+     * @return bool
+     */
+    public function attachAnyDeck(User $user, Content $content): bool
+    {
+        return $user->isAdmin() || $user->isOwner($content);
+    }
+
+    /**
+     * Determine whether the user can add deck to the content.
+     *
+     * @param User $user
+     * @param Content $content
+     * @return bool
+     */
+    public function addDeck(User $user, Content $content): bool
+    {
+        return $user->isAdmin() || $user->isOwner($content);
+    }
+
+    /**
+     * Determine whether the user can attach any adventure to the content.
+     *
+     * @param User $user
+     * @param Content $content
+     * @return bool
+     */
+    public function attachAnyAdventure(User $user, Content $content): bool
+    {
+        return $user->isAdmin() || $user->isOwner($content);
+    }
+
+    /**
+     * Determine whether the user can add adventure to the content.
+     *
+     * @param User $user
+     * @param Content $content
+     * @return bool
+     */
+    public function addAdventure(User $user, Content $content): bool
+    {
+        return $user->isAdmin() || $user->isOwner($content);
+    }
 }

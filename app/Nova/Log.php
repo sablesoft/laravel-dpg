e9923072659a5d2afc42c0d2ac5ebeb711589205
Raman Sobal <v-romansobol@4over.com>
2022-11-14 17:32:00 -0300

    create logs table

---
+++ app/Nova/Log.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Fields\DateTime;
+
+class Log extends Resource
+{
+    /**
+     * The logical group associated with the resource.
+     *
+     * @var string
+     */
+    public static $group = 'Play';
+
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static string $model = \App\Models\Log::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'id';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function fields(Request $request): array
+    {
+        return [
+            BelongsTo::make(__('Game'), 'game'),
+            BelongsTo::make(__('Stack'), 'stack'),
+            BelongsTo::make(__('Card'), 'card'),
+            DateTime::make(__('Created At'), 'created_at')
+                ->hideFromIndex()
+                ->hideWhenCreating()->hideWhenUpdating()->sortable(true),
+            DateTime::make(__('Updated At'), 'updated_at')
+                ->hideFromIndex()
+                ->hideWhenCreating()->hideWhenUpdating()->sortable(true)
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function cards(Request $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function filters(Request $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function lenses(Request $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function actions(Request $request): array
+    {
+        return [];
+    }
+}

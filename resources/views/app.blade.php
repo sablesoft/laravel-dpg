6060a161e0c6fc4df710b4681c605afa09e415de
Raman Sobal <v-romansobol@4over.com>
2022-11-18 21:52:28 -0300

    install laravel breeze vue

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>How To Install Vue 3 in Laravel 9 with Vite</title>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    @vite('resources/css/app.css')
-</head>
-<body>
-<div id="app"></div>
+        <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-@vite('resources/js/app.js')
-</body>
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+
+        <!-- Scripts -->
+        @routes
+        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+        @inertiaHead
+    </head>
+    <body class="font-sans antialiased">
+        @inertia
+    </body>
 </html>

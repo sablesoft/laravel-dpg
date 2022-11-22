f92210850cf4c4bfde97b777c3c9c30598d53cbc
Raman Sobal <v-romansobol@4over.com>
2022-11-22 15:35:18 -0300

    update user seeds

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -21,16 +21,16 @@ class UsersTableSeeder extends Seeder
         \DB::table('users')->insert(array (
             0 => 
             array (
-                'id' => 2,
-                'name' => '{"en":"Test"}',
+                'id' => 4,
+                'name' => '{"en":"Vvrn","ru":"\\u0413\\u044b\\u0433\\u043e\\u0440\\u044c"}',
                 'image' => NULL,
-                'email' => 'test@gmail.com',
+                'email' => 'me.vvrn@gmail.com',
                 'email_verified_at' => NULL,
-                'password' => '$2y$10$rUke3HZ0PtOZyhsukkDBWOdMS8zAdFmdCMxGEjxXUbftHtCD46gHq',
-                'language_id' => 1,
-                'remember_token' => 'ijhd4ZYISH2GQtWluWNXBKNABVlEk47V2oFjpdZlTDacnyLzpURdJlpHlVPp',
-                'created_at' => '2022-11-08 23:12:13',
-                'updated_at' => '2022-11-08 23:57:43',
+                'password' => '$2y$10$z64SqlsR6xnDW9uaNGQp/OHtCjsAJg25IwFSBfeqt8jgsPLIlf9S2',
+                'language_id' => 2,
+                'remember_token' => 'uxlvzXhxSSSuzicXUHjrno85c3Dbbc4eyn5B6da8XdkHBTVc3lcEibAxu1VJ',
+                'created_at' => '2022-11-22 18:18:58',
+                'updated_at' => '2022-11-22 18:29:34',
             ),
             1 => 
             array (
@@ -40,10 +40,10 @@ class UsersTableSeeder extends Seeder
                 'email' => 'sable.lair@gmail.com',
                 'email_verified_at' => NULL,
                 'password' => '$2y$10$tqFq6LC1Vniwd30udVbFi.WzT.rsKmrVvUa6rM/4/fbbcrNEWN0hW',
-                'language_id' => 2,
-                'remember_token' => 'epLwYbVPPJVvdbhLWtwBi3rpBLxq9ETyiWeVsrnwCeCJLoYKq8HLyKhqqwJ4',
+                'language_id' => 1,
+                'remember_token' => '00wBIiL4AAiyGsJsN0fxzmT5N7nXDOLfGWdY5RKE3utGTVu9L9RrtvzllXtm',
                 'created_at' => '2022-11-06 05:05:12',
-                'updated_at' => '2022-11-22 00:12:12',
+                'updated_at' => '2022-11-22 18:15:59',
             ),
         ));
         

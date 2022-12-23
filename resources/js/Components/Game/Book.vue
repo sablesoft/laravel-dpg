0a41a4da9608a82b444d9fd3425ce6cc1136e7de
Raman Sobal <v-romansobol@4over.com>
2022-12-23 02:55:33 -0300

    update book component null options

--- resources/js/Components/Game/Book.vue
+++ resources/js/Components/Game/Book.vue
@@ -86,7 +86,7 @@
         </div>
         <div class="book-content">
             <select v-model="game.selectedId" @change="game.selectDome($event)">
-                <option :value="null" disabled>{{ __('Select Dome') }}</option>
+                <option :value="null" disabled>{{ __('Domes') }}</option>
                 <option v-for="dome in game.bookDomes(game.activeBook.id)" :value="dome.id">
                     {{ dome.name }}
                 </option>
@@ -94,7 +94,7 @@
         </div>
         <div class="book-content">
             <select v-model="game.selectedId" @change="game.selectScene($event)">
-                <option :value="null" disabled>{{ __('Select Scene') }}</option>
+                <option :value="null" disabled>{{ __('Scenes') }}</option>
                 <option v-for="scene in game.bookScenes(game.activeBook.id)" :value="scene.id">
                     {{ scene.name }}
                 </option>
@@ -102,7 +102,7 @@
         </div>
         <div class="book-content">
             <select v-model="game.selectedId" @change="game.selectDeck($event)">
-                <option :value="null" disabled>{{ __('Select Deck') }}</option>
+                <option :value="null" disabled>{{ __('Decks') }}</option>
                 <option v-for="deck in game.bookDecks(game.activeBook.id)" :value="deck.id">
                     {{ deck.target + ' - ' + deck.scope }}
                 </option>
@@ -110,7 +110,7 @@
         </div>
         <div class="book-content">
             <select v-model="game.selectedId" @change="game.selectCard($event)">
-                <option :value="null" disabled>{{ __('Select Card') }}</option>
+                <option :value="null" disabled>{{ __('Cards') }}</option>
                 <option v-for="card in game.bookCards(game.activeBook.id)" :value="card.id">
                     {{ card.name }}
                 </option>

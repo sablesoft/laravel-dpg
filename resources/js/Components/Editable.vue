f178007ec31f8dc3fddce493e59d344043608f15
Raman Sobal <v-romansobol@4over.com>
2023-04-03 00:16:20 -0300

    create Editable.vue component

---
+++ resources/js/Components/Editable.vue
@@ -0,0 +1,41 @@
+<script setup>
+import {nextTick, ref, shallowRef} from "vue";
+
+const emit = defineEmits(['updated']);
+const props = defineProps(['text', 'resource']);
+
+const textarea = ref(null);
+const editMode = shallowRef(false);
+const cache = shallowRef(null);
+
+let edit = function() {
+    editMode.value = !editMode.value;
+    if (editMode.value) {
+        cache.value = props.text;
+        nextTick(() => {
+            textarea.value.focus();
+        });
+    } else {
+        if (cache.value !== props.text) {
+            emit('updated', props.text);
+        }
+    }
+}
+</script>
+
+<style>
+textarea {
+    width: 100%;
+}
+</style>
+
+<template>
+    <template v-if="editMode">
+        <textarea v-model="props.text" ref="textarea" @blur="edit"/>
+    </template>
+    <template v-else>
+        <span @dblclick="edit">
+          {{ text }}
+        </span>
+    </template>
+</template>

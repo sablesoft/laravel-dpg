27cf353d868da4edf50c07d5284a0a467405d338
Raman Sobal <v-romansobol@4over.com>
2023-05-14 11:52:00 -0300

    clear questjs test game

--- resources/js/Games/test/data.js
+++ resources/js/Games/test/data.js
@@ -10,190 +10,186 @@ import {
     Exit, lang, w, Link, world, tp, INDEFINITE
 } from "questjs-core";
 
-// export default function () {
+createItem("me", PLAYER(), {
+  loc:"lounge",
+  synonyms:['me', 'myself'],
+  examine: "The most ordinary guy",
+});
 
-    createItem("me", PLAYER(), {
-      loc:"lounge",
-      synonyms:['me', 'myself'],
-      examine: "The most ordinary guy",
-    });
+// ============================
+// Lounge
+// ============================
 
-    // ============================
-    // Lounge
-    // ============================
+createRoom("lounge", {
+  east: new Link("kitchen"),
+  // down: new Exit('maze_0_0'),
+  desc: "A smelly room with an old settee and a {popup:tv:It looks like this TV is broken...}.",
+  mapColour:'red',
+  mapWidth:45,
+  mapHeight:45,
+});
 
-    createRoom("lounge", {
-      east: new Link("kitchen"),
-      // down: new Exit('maze_0_0'),
-      desc: "A smelly room with an old settee and a {popup:tv:It looks like this TV is broken...}.",
-      mapColour:'red',
-      mapWidth:45,
-      mapHeight:45,
-    });
+// NPC:
+createItem("Lara", NPC(true), {
+  loc:"lounge",
+  examine:"A normal-sized rabbit.",
+})
 
-    // NPC:
-    createItem("Lara", NPC(true), {
-      loc:"lounge",
-      examine:"A normal-sized rabbit.",
-    })
+// Items:
+createItem("coin", TAKEABLE(),  {
+  loc:"lounge",
+  examine: "A gold coin.",
+});
+createItem("ornate_doll", TAKEABLE(), {
+  loc:"glass_cabinet",
+  examine:"A fancy doll, eighteenth century.",
+});
+createItem("boots", WEARABLE(), {
+  loc:"lounge",
+  pronouns: lang.pronouns.plural,
+  examine:"Some old boots.",
+});
+createItem("torch", TAKEABLE(), SWITCHABLE(false, 'providing light'), {
+  loc:"lounge",
+  examine:"A small red torch.",
+  synonyms:["flashlight"],
+  lightSource:function() {
+    return this.switchedon ? world.LIGHT_FULL : world.LIGHT_NONE
+  },
+  eventPeriod:1,
+  eventIsActive:function() {
+    return this.switchedon
+  },
+  eventScript:function() {
+    this.power--;
+    if (this.power === 2) {
+      msg("The torch flickers.")
+    }
+    if (this.power < 0) {
+      msg("The torch flickers and dies.{once: Perhaps there is a charger in the garage?}");
+      this.doSwitchoff()
+    }
+  },
+  testSwitchOn () {
+    if (this.power < 0) {
+      msg("The torch is dead.")
+      return false
+    }
+    return true
+  },
+  power: 3,
+  charge:function(options) {
+    if (options.char.loc !== "garage") return falsemsg("There is nothing to charge the torch with here.")
 
-    // Items:
-    createItem("coin", TAKEABLE(),  {
-      loc:"lounge",
-      examine: "A gold coin.",
-    });
-    createItem("ornate_doll", TAKEABLE(), {
-      loc:"glass_cabinet",
-      examine:"A fancy doll, eighteenth century.",
-    });
-    createItem("boots", WEARABLE(), {
-      loc:"lounge",
-      pronouns: lang.pronouns.plural,
-      examine:"Some old boots.",
-    });
-    createItem("torch", TAKEABLE(), SWITCHABLE(false, 'providing light'), {
-      loc:"lounge",
-      examine:"A small red torch.",
-      synonyms:["flashlight"],
-      lightSource:function() {
-        return this.switchedon ? world.LIGHT_FULL : world.LIGHT_NONE
-      },
-      eventPeriod:1,
-      eventIsActive:function() {
-        return this.switchedon
-      },
-      eventScript:function() {
-        this.power--;
-        if (this.power === 2) {
-          msg("The torch flickers.")
-        }
-        if (this.power < 0) {
-          msg("The torch flickers and dies.{once: Perhaps there is a charger in the garage?}");
-          this.doSwitchoff()
-        }
-      },
-      testSwitchOn () {
-        if (this.power < 0) {
-          msg("The torch is dead.")
-          return false
-        }
-        return true
-      },
-      power: 3,
-      charge:function(options) {
-        if (options.char.loc !== "garage") return falsemsg("There is nothing to charge the torch with here.")
+    msg("{pv:char:charge:true} the torch - it should last for hours now.", options)
+    this.power = 20
+    return true
+  },
+});
+createItem("glass_cabinet", CONTAINER(true), LOCKED_WITH("cabinet_key"), {
+  examine:"A cabinet with a glass front.",
+  loc:"lounge",
+  transparent:true,
+});
+createItem("garage_key", KEY(), {
+  loc:"lounge",
+  examine: "A big key.",
+})
 
-        msg("{pv:char:charge:true} the torch - it should last for hours now.", options)
-        this.power = 20
-        return true
-      },
-    });
-    createItem("glass_cabinet", CONTAINER(true), LOCKED_WITH("cabinet_key"), {
-      examine:"A cabinet with a glass front.",
-      loc:"lounge",
-      transparent:true,
-    });
-    createItem("garage_key", KEY(), {
-      loc:"lounge",
-      examine: "A big key.",
-    })
+// ============================
+// Kitchen
+// ============================
 
-    // ============================
-    // Kitchen
-    // ============================
+createRoom("kitchen", {
+  desc:'A clean room.',
+  north:new Link("garage"),
+  down:new Exit('basement', {
+    isHidden:function() { return w.trapdoor.closed; },
+  }),
+  afterEnterFirst:function() {
+    msg("A fresh smell here!")
+  },
+  exit_locked_north:true,
+});
 
-    createRoom("kitchen", {
-      desc:'A clean room.',
-      north:new Link("garage"),
-      down:new Exit('basement', {
-        isHidden:function() { return w.trapdoor.closed; },
-      }),
-      afterEnterFirst:function() {
-        msg("A fresh smell here!")
-      },
-      exit_locked_north:true,
-    });
+// Way:
+createItem("garage_door", LOCKED_DOOR("garage_key", "kitchen", "garage"), {
+  examine: "The door to the garage.",
+})
 
-    // Way:
-    createItem("garage_door", LOCKED_DOOR("garage_key", "kitchen", "garage"), {
-      examine: "The door to the garage.",
-    })
+// Items:
+createItem("trapdoor", OPENABLE(false), {
+  loc:"kitchen",
+  examine:"A small trapdoor in the floor.",
+})
 
-    // Items:
-    createItem("trapdoor", OPENABLE(false), {
-      loc:"kitchen",
-      examine:"A small trapdoor in the floor.",
-    })
+// ============================
+// Garage
+// ============================
 
-    // ============================
-    // Garage
-    // ============================
+createRoom("garage", {
+  desc:'An empty garage.',
+});
 
-    createRoom("garage", {
-      desc:'An empty garage.',
-    });
+// Items:
+createItem("cabinet_key", KEY(), {
+  loc:"garage",
+  examine: "A small brass key."
+});
+createItem("charger", {
+  loc:"garage",
+  examine: "A device bigger than a washing machine to charge a torch? It has a compartment and a button. {charger_state}.",
+});
+createItem("charger_compartment", COMPONENT("charger"), CONTAINER(true), {
+  alias:"compartment",
+  examine:"The compartment is just the right size for the torch. It is {if:charger_compartment:closed:closed:open}.",
+  testDropIn:function(options) {
+    const contents = w.charger_compartment.getContents();
+    if (contents.length > 0) return falsemsg("The compartment is full.")
 
-    // Items:
-    createItem("cabinet_key", KEY(), {
-      loc:"garage",
-      examine: "A small brass key."
-    });
-    createItem("charger", {
-      loc:"garage",
-      examine: "A device bigger than a washing machine to charge a torch? It has a compartment and a button. {charger_state}.",
-    });
-    createItem("charger_compartment", COMPONENT("charger"), CONTAINER(true), {
-      alias:"compartment",
-      examine:"The compartment is just the right size for the torch. It is {if:charger_compartment:closed:closed:open}.",
-      testDropIn:function(options) {
-        const contents = w.charger_compartment.getContents();
-        if (contents.length > 0) return falsemsg("The compartment is full.")
+    return true
+  },
+});
+createItem("charger_button", COMPONENT("charger"), {
+  examine:"A big red button.",
+  alias:"button",
+  push:function(options) {
+    if (!w.charger_compartment.closed || w.torch.loc !== "charger_compartment") return falsemsg("{pv:char:push:true} the button, but nothing happens.", options)
 
-        return true
-      },
-    });
-    createItem("charger_button", COMPONENT("charger"), {
-      examine:"A big red button.",
-      alias:"button",
-      push:function(options) {
-        if (!w.charger_compartment.closed || w.torch.loc !== "charger_compartment") return falsemsg("{pv:char:push:true} the button, but nothing happens.", options)
+    msg("{pv:char:push:true} the button. There is a brief hum of power, and a flash.", options)
+    w.torch.power = 20
+    return true
+  },
+});
+tp.addDirective("charger_state", function() {
+  if (w.charger_compartment.closed) {
+    return "The compartment is closed"
+  }
+  const contents = w.charger_compartment.getContents()
+  if (contents.length === 0) {
+    return "The compartment is empty"
+  }
+  return "The compartment contains " + formatList(contents, {article:INDEFINITE, lastJoiner:'and'})
+})
 
-        msg("{pv:char:push:true} the button. There is a brief hum of power, and a flash.", options)
-        w.torch.power = 20
-        return true
-      },
-    });
-    tp.addDirective("charger_state", function() {
-      if (w.charger_compartment.closed) {
-        return "The compartment is closed"
-      }
-      const contents = w.charger_compartment.getContents()
-      if (contents.length === 0) {
-        return "The compartment is empty"
-      }
-      return "The compartment contains " + formatList(contents, {article:INDEFINITE, lastJoiner:'and'})
-    })
+// ============================
+// Basement
+// ============================
 
-    // ============================
-    // Basement
-    // ============================
+createRoom("basement", {
+  desc:"A dank room, with piles of crates everywhere.",
+  darkDesc:"It is dark, but you can just see the outline of the trapdoor above you.",
+  up:new Exit('kitchen', {
+    illuminated:true,
+  }),
+  lightSource:function() {
+    return w.light_switch.switchedon ? world.LIGHT_FULL : world.LIGHT_NONE;
+  },
+});
 
-    createRoom("basement", {
-      desc:"A dank room, with piles of crates everywhere.",
-      darkDesc:"It is dark, but you can just see the outline of the trapdoor above you.",
-      up:new Exit('kitchen', {
-        illuminated:true,
-      }),
-      lightSource:function() {
-        return w.light_switch.switchedon ? world.LIGHT_FULL : world.LIGHT_NONE;
-      },
-    });
-
-    // Items:
-    createItem("light_switch", SWITCHABLE(false), {
-      loc:"basement",
-      alias:"light switch",
-      examine:"A switch, presumably for the light.",
-    });
-
-// }
+// Items:
+createItem("light_switch", SWITCHABLE(false), {
+  loc:"basement",
+  alias:"light switch",
+  examine:"A switch, presumably for the light.",
+});
--- resources/js/Games/test/settings.js
+++ resources/js/Games/test/settings.js
@@ -1,64 +1,61 @@
 "use strict"
 
 import { settings } from "questjs-core";
-import {set} from "lodash/object";
 
-// export default function () {
-    settings.title = "Quest 6 Testing"
-    settings.author = "Ziggy Shu"
-    settings.version = "0.1"
-    settings.thanks = []
-    settings.warnings = "No warnings have been set for this game."
-    settings.playMode = "dev"
-    settings.lang = "lang-en"
-    settings.files = ["code", "data"];
-    // settings.collapsibleSidePanes = true;
+settings.title = "Quest 6 Testing"
+settings.author = "Ziggy Shu"
+settings.version = "0.1"
+settings.thanks = []
+settings.warnings = "No warnings have been set for this game."
+settings.playMode = "dev"
+settings.lang = "lang-en"
+settings.files = ["code", "data"];
+// settings.collapsibleSidePanes = true;
 
-    settings.mapShowNotVisited = false
-    settings.mapCellSize = 55
-    settings.mapScale = 60
-    // settings.mapLocationColour = 'black'
-    // settings.mapBorderColour = 'white'
-    // settings.mapExitColour = 'blue'
-    settings.mapExitWidth = 7
-    settings.mapStyle = {
-        right:'10px',
-        top:'10px',
-        width:'400px',
-        height:'400px',
-        'background-color':'#ddd',
-        'background-image':'url(assets/images/paper2.jpeg)',
-        border:'3px black solid',
-    }
-    settings.mapDrawLabels = true
-    settings.mapLabelStyle = {'font-size':'6pt', 'font-weight':'bold'}
-    settings.mapLabelColour = 'blue'
-    settings.mapLabelOffset = -5
-    settings.mapLabelRotate = -20
-    settings.mapMarker = function(loc) {
-        return map.polygon(loc, [
-            [0,0], [-5,-25], [-7, -20], [-18, -45], [-20, -40], [-25, -42], [-10, -18], [-15, -20]
-        ], 'stroke:none;fill:black;pointer-events:none;opacity:0.3')
-    }
-    settings.mapExtras = function() {
-        const result = []
-        const room = w[player.loc]
-        result.push(map.polygon(room, [
-            [150, 100],
-            [147, 117],
-            [130, 120],
-            [147, 123],
-            [150, 160],
-            [153, 123],
-            [170, 120],
-            [153, 117],
-        ], 'stroke:black;fill:yellow;'))
-        result.push(map.text(room, 'N', [150, 95], 'fill:black;font-size:14pt'))
-        return result
-    }
-    settings.mapClick = function(name) {
-        console.log("Map clicked: " + name)
-    }
+settings.mapShowNotVisited = false
+settings.mapCellSize = 55
+settings.mapScale = 60
+// settings.mapLocationColour = 'black'
+// settings.mapBorderColour = 'white'
+// settings.mapExitColour = 'blue'
+settings.mapExitWidth = 7
+settings.mapStyle = {
+    right:'10px',
+    top:'10px',
+    width:'400px',
+    height:'400px',
+    'background-color':'#ddd',
+    'background-image':'url(assets/images/paper2.jpeg)',
+    border:'3px black solid',
+}
+settings.mapDrawLabels = true
+settings.mapLabelStyle = {'font-size':'6pt', 'font-weight':'bold'}
+settings.mapLabelColour = 'blue'
+settings.mapLabelOffset = -5
+settings.mapLabelRotate = -20
+settings.mapMarker = function(loc) {
+    return map.polygon(loc, [
+        [0,0], [-5,-25], [-7, -20], [-18, -45], [-20, -40], [-25, -42], [-10, -18], [-15, -20]
+    ], 'stroke:none;fill:black;pointer-events:none;opacity:0.3')
+}
+settings.mapExtras = function() {
+    const result = []
+    const room = w[player.loc]
+    result.push(map.polygon(room, [
+        [150, 100],
+        [147, 117],
+        [130, 120],
+        [147, 123],
+        [150, 160],
+        [153, 123],
+        [170, 120],
+        [153, 117],
+    ], 'stroke:black;fill:yellow;'))
+    result.push(map.text(room, 'N', [150, 95], 'fill:black;font-size:14pt'))
+    return result
+}
+settings.mapClick = function(name) {
+    console.log("Map clicked: " + name)
+}
 
-    settings.maze_size = 5;
-// }
+settings.maze_size = 5;
